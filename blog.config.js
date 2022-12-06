451f8a340ae8b77f71830ef7ea2da31c583db477
zunx <zhangshun333@163.com>
2022-12-07 01:04:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '顺+仔', // 作者
   BIO: '一个普通的老实人🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://zs.zunx.net/', // 网站地址
   KEYWORDS: '仲夏, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
